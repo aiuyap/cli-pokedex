ff35c8629a2ea1de0c7912a9409f76362e9933aa
Aiu <186591613+aiuyap@users.noreply.github.com>
2026-03-07 05:41:51 +0800

    feat: implemented input on startREPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,25 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(str: string): string[] {
   const arr = str.toLowerCase().trim();
   if (arr === "") return [];
   return arr.split(/\s+/);
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (input) => {
+    const cleaned = cleanInput(input);
+    if (!cleaned.length) {
+      rl.prompt();
+      return;
+    }
+    console.log(`Your command was: ${cleaned[0]}`);
+    rl.prompt();
+  });
 }
